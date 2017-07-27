eee179b6cada6d67772c63125458008a0d84be15
Paul Trott <ptrott@theblaze.com>
2017-07-26 21:03:45 -0500

    added lecture 29 sample

---
+++ lecture29/lib2.js
@@ -0,0 +1,5 @@
+// Stop Clashing
+// Framework Default Values
+// Javascript Frameworks could collide with one another
+// Should check Global Object before name collions are caused, exclude yourself, and don't override if it exists
+window.libraryName = window.libraryName || "Lib 2";
